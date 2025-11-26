39948f56b47bff06734cbf4d873e34ad2907d555
Ahnaf Batayneh <ahnafbataineh97@gmail.com>
2025-11-27 01:08:48 +0300

    update

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 // We no longer send a JWT; just connect to the server
-export const SERVER_URL = "https://realtime-tracker-4aed.onrender.com";
+export const SERVER_URL = "http://localhost:3020";
 // export const SERVER_URL = "http://localhost:3020";
 
 export const socket = io(SERVER_URL, {
